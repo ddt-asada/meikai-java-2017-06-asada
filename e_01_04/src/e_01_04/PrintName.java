d8ba33dbfc1e60bf092176cf88c1c16102f01862
ddt-asada <ddt.asada@gmail.com>
2017-06-16 19:48:18 +0900

    java 演習1-4 100％ 初回コミット （浅田

---
+++ e_01_04/src/e_01_04/PrintName.java
@@ -0,0 +1,14 @@
+/*演習1-4
+ * 各行に自分の名前を1文字ずつ表示するプログラム。
+ * 作成日：2017年6月16日
+ * 作成者：浅田　知嗣
+ */
+package e_01_04;
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		System.out.println("浅\n田\n知\n嗣\n");
+	}
+
+}
