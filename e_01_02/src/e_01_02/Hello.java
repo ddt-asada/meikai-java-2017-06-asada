59b20d3390fb5dfd616f89aa2790e60e2a6efd8c
ddt-asada <ddt.asada@gmail.com>
2017-06-16 19:48:05 +0900

    java 演習1-2 100％ 初回コミット （浅田

---
+++ e_01_02/src/e_01_02/Hello.java
@@ -0,0 +1,16 @@
+/*演習1-2
+ * プログラムの分の終端を示す;が欠如しているとどうなるかを調べる。
+ * 作成日：2017年6月16日
+ * 作成者：浅田　知嗣
+ */
+
+package e_01_02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("初めてのjavaプログラム。");
+		System.out.println("画面に出力しています。")
+	}
+
+}
