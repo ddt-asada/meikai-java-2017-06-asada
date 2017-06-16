66d2047993e10432eb2e49a0619e47b082d3fa9b
ddt-asada <ddt.asada@gmail.com>
2017-06-16 19:48:11 +0900

    java 演習1-3 100％ 初回コミット （浅田

---
+++ e_01_03/src/e_01_03/Hello.java
@@ -0,0 +1,15 @@
+/*演習1-3
+ * 改行することなく連続表示するプログラムを作成する。
+ * 作成日：2017年6月16日
+ * 作成者：浅田　知嗣
+ */
+package e_01_03;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.print("初めてのjavaプログラム。");
+		System.out.println("画面に出力しています。");
+	}
+
+}
