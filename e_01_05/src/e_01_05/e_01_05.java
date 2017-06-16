a35a3ab5dd882c0629210f37536058100ff4f46a
ddt-asada <ddt.asada@gmail.com>
2017-06-16 19:48:25 +0900

    java 演習1-5 100％ 初回コミット （浅田

---
+++ e_01_05/src/e_01_05/e_01_05.java
@@ -0,0 +1,14 @@
+/*演習1-5
+ * 各行に自分の名前を1文字ずつ表示するプログラム。
+ * 作成日：2017年6月16日
+ * 作成者：浅田　知嗣
+ */
+package e_01_05;
+
+public class e_01_05 {
+
+	public static void main(String[] args) {
+		System.out.println("浅\n田\n\n知\n嗣\n");
+	}
+
+}
