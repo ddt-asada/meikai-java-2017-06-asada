568e820b6a0cdb1f2a5ca9edd3940f30a15edde6
ddt-asada <ddt.asada@gmail.com>
2017-06-16 19:57:03 +0900

    java 演習2-1 100％ 初回コミット （浅田

---
+++ e_02_01/src/e_02_01/e_02_01.java
@@ -0,0 +1,17 @@
+/*演習2-1
+ * 二つの整数の和と差を求めるプログラムを作成する。
+ */
+package e_02_01;
+
+public class e_02_01 {
+
+	public static void main(String[] args) {
+		System.out.println("82 + 17 = " + (82 + 17));
+		System.out.println("82 - 17 = " + (82 - 17));
+		System.out.print("82 + 17 = ");
+		System.out.println(82 + 17);
+		System.out.print("82 - 17 = ");
+		System.out.println(82 - 17);
+	}
+
+}
