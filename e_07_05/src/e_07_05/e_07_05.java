c58ab6f38f46e243ea167f1870694eb02c705941
ddt-asada <ddt.asada@gmail.com>
2017-06-20 20:38:49 +0900

    java 演習7-5 100％ 初回コミット （浅田

---
+++ e_07_05/src/e_07_05/e_07_05.java
@@ -0,0 +1,21 @@
+/*演習7-5
+ * こんにちはと表示して改行するメソッド
+ * 作成日：2017年6月20日
+ * 作成者：浅田　知嗣
+ */
+package e_07_05;
+
+public class e_07_05 {
+
+	public static void main(String[] args) {
+		//こんにちはと表示しして改行する関数を呼び出す・
+		hello();
+
+
+	}
+
+	static void hello() {
+		System.out.println("こんにちは。");
+	}
+
+}
