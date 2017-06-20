204b838570a1b0fbf5319cc1816197e3c7a6f609
ddt-asada <ddt.asada@gmail.com>
2017-06-20 10:47:42 +0900

    java 演習5-3 100％ 初回コミット （浅田

---
+++ e_05_03/src/e_05_03/e_05_03.java
@@ -0,0 +1,19 @@
+/*演習5-3
+ * 論理型の変数にtrueとfalseを代入してその値を表示するプログラム。
+ * 作成日：2017年6月20日
+ * 作成者：浅田　知嗣
+ */
+package e_05_03;
+
+public class e_05_03 {
+
+	public static void main(String[] args) {
+		boolean btest1 = true;	//bool型にtrueを代入。
+		boolean btest2 = false;//bool型にfalseを代入。
+
+		//代入した値を表示して確かめる。
+		System.out.println("btest1 = " + btest1);
+		System.out.println("btest2 = " + btest2);
+	}
+
+}
